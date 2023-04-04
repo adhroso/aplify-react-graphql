cb69ad9c70251c3a405b56f6edc6702f0625b431
Andi Dhroso <andidhroso@Andi-Dhroso.lan>
2023-04-04 09:04:19 -0400

    fix app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
-import { API } from "aws-amplify";
-import {
-  Button,
-  Flex,
-  Heading,
-  Text,
-  TextField,
-  View,
-  withAuthenticator,
-} from "@aws-amplify/ui-react";
 import { listNotes } from "./graphql/queries";
 import {
   createNote as createNoteMutation,
@@ -82,6 +72,7 @@ const App = ({ signOut }) => {
   return (
     <View className="App">
       <Heading level={1}>My Notes App</Heading>
+
       <View as="form" margin="3rem 0" onSubmit={createNote}>
         <Flex direction="row" justifyContent="center">
           <TextField
@@ -105,7 +96,9 @@ const App = ({ signOut }) => {
           </Button>
         </Flex>
       </View>
+
       <Heading level={2}>Current Notes</Heading>
+
       <View margin="3rem 0">
         {notes.map((note) => (
           <Flex
@@ -125,37 +118,37 @@ const App = ({ signOut }) => {
         ))}
       </View>
       <Button onClick={signOut}>Sign Out</Button>
-    </View>
-    <View
-	  name="image"
-	  as="input"
-	  type="file"
-	  style={{ alignSelf: "end" }}
-	/>
+      <View
+  		name="image"
+		as="input"
+  		type="file"
+  		style={{ alignSelf: "end" }}
+	  />
 
-	{notes.map((note) => (
-	  <Flex
-		key={note.id || note.name}
-		direction="row"
-		justifyContent="center"
-		alignItems="center"
-	  >
-		<Text as="strong" fontWeight={700}>
-		  {note.name}
-		</Text>
-		<Text as="span">{note.description}</Text>
-		{note.image && (
-		  <Image
-			src={note.image}
-			alt={`visual aid for ${notes.name}`}
-			style={{ width: 400 }}
-		  />
-		)}
-		<Button variation="link" onClick={() => deleteNote(note)}>
-		  Delete note
-		</Button>
-	  </Flex>
-	))}
+		{notes.map((note) => (
+		  <Flex
+			key={note.id || note.name}
+			direction="row"
+			justifyContent="center"
+			alignItems="center"
+		  >
+			<Text as="strong" fontWeight={700}>
+			  {note.name}
+			</Text>
+			<Text as="span">{note.description}</Text>
+			{note.image && (
+			  <Image
+				src={note.image}
+				alt={`visual aid for ${notes.name}`}
+				style={{ width: 400 }}
+			  />
+			)}
+			<Button variation="link" onClick={() => deleteNote(note)}>
+			  Delete note
+			</Button>
+		  </Flex>
+		))}
+  </View>
   );
 };
 
